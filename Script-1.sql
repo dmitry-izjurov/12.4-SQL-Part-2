58604bf35f0acb6bcd5b4373cda2fa91d9e8262d
Desilver <dmitry.izjurov@yandex.ru>
2021-10-16 13:30:40 +0300

    Исправлена кодировка

--- Script-1.sql
+++ Script-1.sql
@@ -1,8 +1,9 @@
 -- Task 1
-SELECT st.first_name 'Имя сотрудника',
-st.last_name 'Фамилия сотрудника',
-c.city 'Город', cus.store_id 'Номер магазина',
-count(cus.store_id) 'Количество покупателей'
+SELECT st.first_name 'Name',
+st.last_name 'Lastname',
+c.city 'City',
+cus.store_id 'Number of store',
+count(cus.store_id) 'Number of buyers'
 FROM store s
 JOIN address a ON s.address_id = a.address_id
 JOIN city c ON c.city_id = a.city_id
@@ -12,13 +13,13 @@ GROUP BY cus.store_id
 HAVING count(cus.store_id) > 300;
 
 -- Task 2
-SELECT count(f.`length`) 'Количество фильмов >AVG' FROM film f
+SELECT count(f.`length`) 'Number of films >AVG' FROM film f
 WHERE f.`length` > (SELECT avg(f2.`length`) FROM film f2);
 
 -- Task 3
-SELECT MONTH(p.payment_date) 'Месяц',
-sum(p.amount) 'Наибольшая сумма',
-count(p.payment_id) 'Количество аренд'
+SELECT MONTH(p.payment_date) 'Month',
+sum(p.amount) 'The largest amount',
+count(p.payment_id) 'Number of rentals'
 FROM payment p
 GROUP BY MONTH(p.payment_date)
 ORDER BY sum(p.amount) DESC
